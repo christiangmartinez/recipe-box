ad40e2e33907867c7ed7e535af6a1115c977dc80
Christian Martinez and Shane Stafford <shane@>
2017-04-05 11:50:28 -0700

    update with instructor class and mostly completed tests

--- src/main/java/Instruction.java
+++ src/main/java/Instruction.java
@@ -8,9 +8,82 @@ import java.util.TimerTask;
 import java.sql.Timestamp;
 
 public class Instruction {
+  private String name;
+  private int id;
+  private int recipe_id;
 
-  public Instruction() {
+  public Instruction(String name) {
+    this.name = name;
+    this.recipe_id = recipe_id;
+  }
 
+  public String getInstructionName() {
+    return name;
+  }
+
+  public int getInstructionId() {
+    return id;
+  }
+
+  public int getInstructionInstructionId() {
+    return recipe_id;
+  }
+
+  @Override
+  public boolean equals(Object otherInstruction) {
+      if (!(otherInstruction instanceof Instruction)) {
+        return false;
+      } else {
+        Instruction newInstruction = (Instruction) otherInstruction;
+        return this.getInstructionName().equals(newInstruction.getInstructionName()) && this.getInstructionId() == newInstruction.getInstructionId();
+      }
+  }
+
+  public static List<Instruction> allInstructions() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM instructions;";
+      return con.createQuery(sql)
+        .executeAndFetch(Instruction.class);
+    }
+  }
+
+  public void saveInstruction() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO instructions (name) VALUES (:name);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Instruction findInstruction(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM instructions WHERE id = :id;";
+      Instruction newInstruction  = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Instruction.class);
+    return newInstruction;
+    }
+  }
+
+  public void updateInstruction(String name) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE instructions SET name = :name WHERE id = :id;";
+      con.createQuery(sql)
+      .addParameter("name", name)
+      .addParameter("id", this.id)
+      .executeUpdate();
+    }
+  }
+
+  public void deleteInstruction() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM instructions WHERE id = :id;";
+      con.createQuery(sql)
+      .addParameter("id", this.id)
+      .executeUpdate();
+    }
   }
 
 }
--- src/test/java/InstructionTest.java
+++ src/test/java/InstructionTest.java
@@ -10,8 +10,70 @@ import java.util.TimerTask;
 import java.sql.Timestamp;
 
 public class InstructionTest {
+  private Instruction testInstruction;
+
+  @Before
+  public void setUp() {
+    testInstruction = new Instruction("Bake");
+    testInstruction.saveInstruction();
+  }
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void createsNewInstruction_True() {
+    assertTrue(testInstruction instanceof Instruction);
+  }
+
+  @Test
+  public void getters_returnObjectVariables() {
+    assertEquals("Bake", testInstruction.getInstructionName());
+  }
+
+  @Test
+  public void returnsAllInstructions_True() {
+    Instruction testInstruction2 = new Instruction("Bake");
+    testInstruction2.saveInstruction();
+    assertTrue(Instruction.allInstructions().contains(testInstruction2));
+  }
+
+  @Test
+  public void overrideEqualsWorks_True() {
+    Instruction testInstruction2 = Instruction.findInstruction(testInstruction.getInstructionId());
+    assertTrue(testInstruction.equals(testInstruction2));
+  }
+
+  @Test
+  public void savesAllInstructions_True() {
+    Instruction testInstruction2 = new Instruction("Bake");
+    testInstruction2.saveInstruction();
+    assertTrue(testInstruction2.equals(Instruction.allInstructions().get(1)));
+  }
+
+   @Test
+   public void instructionsAreCreatedWithId_True() {
+     assertTrue(testInstruction.getInstructionId() > 0);
+   }
+
+   @Test
+   public void findsInstructionBasedOnId_TR2() {
+     Instruction testInstruction2 = new Instruction("Bake");
+     testInstruction2.saveInstruction();
+     assertEquals(testInstruction2, Instruction.findInstruction(testInstruction2.getInstructionId()));
+   }
+
+   @Test
+   public void updatesInstructionInformation_True() {
+     testInstruction.updateInstruction("Cook");
+     assertEquals(Instruction.findInstruction(testInstruction.getInstructionId()).getInstructionName(), "Cook");
+   }
+
+   @Test
+   public void deleteInstruction_deletesInstructionFromDatabase_true() {
+     int testInstructionId = testInstruction.getInstructionId();
+     testInstruction.deleteInstruction();
+     assertEquals(null, Instruction.findInstruction(testInstructionId));
+   }
 
 }
